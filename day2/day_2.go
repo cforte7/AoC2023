62ed595436c9c8eb04379728ac8bf3851413400e
desktop <cforte7@gmail.com>
2023-12-02 10:13:18 -0600

    make it better

--- day2/day_2.go
+++ day2/day_2.go
@@ -8,46 +8,13 @@ import (
 	"strings"
 )
 
-var lookup = map[string]int{
+var max_per_game = map[string]int{
 	"red":   12,
 	"green": 13,
 	"blue":  14,
 }
 
-func eval_game(game string) bool {
-	arr_of_dice := strings.Split(game, ", ")
-	for _, die := range arr_of_dice {
-		res := strings.Split(die, " ")
-		roll_count, _ := strconv.Atoi(res[0])
-		if lookup[res[1]] < roll_count {
-			return false
-		}
-	}
-	return true
-}
-
-func evaluate_games(games string) bool {
-	arr_of_games := strings.Split(games, "; ")
-	for _, game := range arr_of_games {
-		if !eval_game(game) {
-			return false
-		}
-	}
-	return true
-}
-
-func parse_line_part_one(line string) int {
-	res := strings.Split(line, ": ")
-	game_title := strings.Split(res[0], " ")
-	game_num := game_title[1]
-	if evaluate_games(res[1]) {
-		as_num, _ := strconv.Atoi(game_num)
-		return as_num
-	}
-	return 0
-}
-
-func parse_line_part_two(line string) int {
+func get_max_per_line(line string) map[string]int {
 	res := strings.Split(line, ": ")
 	arr_of_games := strings.Split(res[1], "; ")
 	var max_die = map[string]int{
@@ -65,7 +32,24 @@ func parse_line_part_two(line string) int {
 			max_die[color] = max(max_die[color], count)
 		}
 	}
-	return max_die["red"] * max_die["green"] * max_die["blue"]
+	return max_die
+}
+
+func game_is_valid(max_lookup map[string]int) bool {
+	for key, val := range max_per_game {
+		if max_lookup[key] > val {
+			return false
+		}
+	}
+	return true
+}
+
+func calc_power_level(max_lookup map[string]int) int {
+	power := 1
+	for _, val := range max_lookup {
+		power *= val
+	}
+	return power
 }
 
 func main() {
@@ -74,11 +58,13 @@ func main() {
 	sum_one := 0
 	sum_two := 0
 	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		sum_one += parse_line_part_one(scanner.Text())
-		sum_two += parse_line_part_two(scanner.Text())
+	for lineNumber := 1; scanner.Scan(); lineNumber++ {
+		max_lookup := get_max_per_line(scanner.Text())
+		if game_is_valid(max_lookup) {
+			sum_one += lineNumber
+		}
+		sum_two += calc_power_level(max_lookup)
 	}
-
 	fmt.Printf("Part one: %d\n", sum_one)
-	fmt.Printf("Part two: %d", sum_two)
+	fmt.Printf("Part two: %d\n", sum_two)
 }
